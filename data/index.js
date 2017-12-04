faa635b81733d3f63450d2929b2b069384de07d6
Trommelochse <clemens.a.janes@gmail.com>
2017-12-04 12:57:52 +0100

    added brand data

---
+++ data/index.js
@@ -0,0 +1,240 @@
+const brandData = {
+  betsson: {
+    en: {
+      ca: {
+        web: 'https://casino.betsson.com/sv/',
+        mob: 'https://m.betsson.com/en/casino',
+      },
+      sb: {
+        web: 'https://sportsbook.betsson.com/en/',
+        mob: 'https://m.betsson.com/en/sportsbook',
+      },
+      lca: {
+        web: 'https://livecasino.betsson.com/sv/',
+        mob: 'https://m.betsson.com/en/live-casino',
+      },
+      startpage: {
+        web: 'https://www.betsson.com/en',
+        mob: 'https://m.betsson.com/en',
+      }
+    },
+    sv: {
+      ca: {
+        web: 'https://casino.betsson.com/sv/',
+        mob: 'https://m.betsson.com/sv/casino',
+      },
+      sb: {
+        web: 'https://sportsbook.betsson.com/sv/',
+        mob: 'https://m.betsson.com/sv/odds',
+      },
+      lca: {
+        web: 'https://livecasino.betsson.com/sv/',
+        mob: 'https://m.betsson.com/sv/live-casino',
+      },
+      startpage: {
+        web: 'https://www.betsson.com/sv',
+        mob: 'https://m.betsson.com/sv',
+      }
+    },
+    no: {
+      ca: {
+        web: 'https://casino.betsson.com/sv/',
+        mob: 'https://m.betsson.com/no/casino',
+      },
+      sb: {
+        web: 'https://sportsbook.betsson.com/no/',
+        mob: 'https://m.betsson.com/no/odds',
+      },
+      lca: {
+        web: 'https://livecasino.betsson.com/sv/',
+        mob: 'https://m.betsson.com/no/live-casino',
+      },
+      startpage: {
+        web: 'https://www.betsson.com/no',
+        mob: 'https://m.betsson.com/no',
+      }
+    },
+    fi: {
+      ca: {
+        web: 'https://casino.betsson.com/sv/',
+        mob: 'https://m.betsson.com/fi/casino',
+      },
+      sb: {
+        web: 'https://sportsbook.betsson.com/fi/',
+        mob: 'https://m.betsson.com/fi/vedonlyonti',
+      },
+      lca: {
+        web: 'https://livecasino.betsson.com/sv/',
+        mob: 'https://m.betsson.com/fi/live-casino',
+      },
+      startpage: {
+        web: 'https://www.betsson.com/fi',
+        mob: 'https://m.betsson.com/fi',
+      }
+    },
+
+  },
+  nordicbet: {
+    en: {
+      ca: {
+        web: 'https://www.nordicbet.com/en/casino',
+        mob: 'https://m.nordicbet.com/en/casino',
+      },
+      sb: {
+        web: 'https://www.nordicbet.com/en/odds',
+        mob: 'https://m.nordicbet.com/en/sportsbook',
+      },
+      lca: {
+        web: 'https://www.nordicbet.com/en/livecasino',
+        mob: 'https://m.nordicbet.com/en/live-casino',
+      },
+      startpage: {
+        web: 'https://www.nordicbet.com/en',
+        mob: 'https://m.nordicbet.com/en',
+      }
+    },
+    sv: {
+      ca: {
+        web: 'https://www.nordicbet.com/sv/casino',
+        mob: 'https://m.nordicbet.com/sv/casino',
+      },
+      sb: {
+        web: 'https://www.nordicbet.com/sv/odds',
+        mob: 'https://m.nordicbet.com/sv/odds',
+      },
+      lca: {
+        web: 'https://www.nordicbet.com/sv/livecasino',
+        mob: 'https://m.nordicbet.com/sv/live-casino',
+      },
+      startpage: {
+        web: 'https://www.nordicbet.com/sv',
+        mob: 'https://m.nordicbet.com/sv',
+      }
+    },
+    no: {
+      ca: {
+        web: 'https://www.nordicbet.com/no/casino',
+        mob: 'https://m.nordicbet.com/no/casino',
+      },
+      sb: {
+        web: 'https://www.nordicbet.com/no/odds',
+        mob: 'https://m.nordicbet.com/no/odds',
+      },
+      lca: {
+        web: 'https://www.nordicbet.com/no/livecasino',
+        mob: 'https://m.nordicbet.com/no/live-casino',
+      },
+      startpage: {
+        web: 'https://www.nordicbet.com/no',
+        mob: 'https://m.nordicbet.com/no',
+      }
+    },
+    fi: {
+      ca: {
+        web: 'https://www.nordicbet.com/fi/casino',
+        mob: 'https://m.nordicbet.com/fi/casino',
+      },
+      sb: {
+        web: 'https://www.nordicbet.com/fi/vedonlyönti',
+        mob: 'https://m.nordicbet.com/fi/vedonlyonti',
+      },
+      lca: {
+        web: 'https://www.nordicbet.com/fi/livecasino',
+        mob: 'https://m.nordicbet.com/fi/live-casino',
+      },
+      startpage: {
+        web: 'https://www.nordicbet.com/fi',
+        mob: 'https://m.nordicbet.com/fi',
+      }
+    },
+
+  },
+  betsafe: {
+    en: {
+      ca: {
+        web: 'https://www.betsafe.com/en/casino',
+        mob: 'https://m.betsafe.com/en/casino',
+      },
+      sb: {
+        web: 'https://www.betsafe.com/en/odds',
+        mob: 'https://m.betsafe.com/en/sportsbook',
+      },
+      lca: {
+        web: 'https://www.betsafe.com/en/live-casino',
+        mob: 'https://m.betsafe.com/en/live-casino',
+      },
+      startpage: {
+        web: 'https://www.betsafe.com/en',
+        mob: 'https://m.betsafe.com/en',
+      }
+    },
+    sv: {
+      ca: {
+        web: 'https://www.betsafe.com/sv/casino',
+        mob: 'https://m.betsafe.com/sv/casino',
+      },
+      sb: {
+        web: 'https://www.betsafe.com/sv/odds',
+        mob: 'https://m.betsafe.com/sv/odds',
+      },
+      lca: {
+        web: 'https://www.betsafe.com/sv/live-casino',
+        mob: 'https://m.betsafe.com/sv/live-casino',
+      },
+      startpage: {
+        web: 'https://www.betsafe.com/sv',
+        mob: 'https://m.betsafe.com/sv',
+      }
+    },
+    no: {
+      ca: {
+        web: 'https://www.betsafe.com/no/casino',
+        mob: 'https://m.betsafe.com/no/casino',
+      },
+      sb: {
+        web: 'https://www.betsafe.com/no/odds',
+        mob: 'https://m.betsafe.com/no/odds',
+      },
+      lca: {
+        web: 'https://www.betsafe.com/no/live-casino',
+        mob: 'https://m.betsafe.com/no/live-casino',
+      },
+      startpage: {
+        web: 'https://www.betsafe.com/no',
+        mob: 'https://m.betsafe.com/no',
+      }
+    },
+    fi: {
+      ca: {
+        web: 'https://www.betsafe.com/fi/casino',
+        mob: 'https://m.betsafe.com/fi/casino',
+      },
+      sb: {
+        web: 'https://www.betsafe.com/fi/odds',
+        mob: 'https://m.betsafe.com/fi/odds',
+      },
+      lca: {
+        web: 'https://www.betsafe.com/fi/live-casino',
+        mob: 'https://m.betsafe.com/fi/live-casino',
+      },
+      startpage: {
+        web: 'https://www.betsafe.com/fi',
+        mob: 'https://m.betsafe.com/fi',
+      }
+    },
+
+  }
+}
+
+const getBrandUrls = function (settings) {
+  const constants = {};
+  const brand = brandData[settings.brand];
+  constants.webUrl = brand[settings.language][settings.product].web;
+  constants.mobUrl = brand[settings.language][settings.product].mob;
+  return constants
+}
+
+module.exports = {
+  brandData,
+  getBrandUrls
+}
